961af74a9740f2cef97c54dbb105fb36e41f23b1
Murphy Studebaker <murphy.stude@gmail.com>
2022-06-12 15:14:12 -0700

    nice lil arrow animation

--- components/StartButton.js
+++ components/StartButton.js
@@ -3,7 +3,22 @@ import styled from "styled-components";
 export default function Start({ handleClick }) {
   return (
     <Link href="/meditating" passHref>
-      <StyledLink onClick={handleClick}>I'm ready to begin ➜</StyledLink>
+      <StyledLink onClick={handleClick}>
+        I'm ready to begin
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth={2}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M13 7l5 5m0 0l-5 5m5-5H6"
+          />
+        </svg>
+      </StyledLink>
     </Link>
   );
 }
@@ -12,8 +27,24 @@ const StyledLink = styled.a`
   background-color: var(--color-darker);
   padding: var(--spacing) calc(2 * var(--spacing));
   border-radius: var(--radius);
+  display: flex;
+  gap: var(--spacing);
+  align-items: center;
+  max-width: fit-content;
+
+  & svg {
+    height: 1em;
+    display: inline;
+    transform: translateX(0px);
+    transition: transform 400ms;
+  }
 
   &:hover {
     text-decoration: none;
+  }
+
+  &:hover svg {
+    transform: translateX(8px);
+    transition: transform 300ms;
   }
 `;
